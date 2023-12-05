0c691b6f1d46bec9b8ca38c443ef6bb2207ca040
J. A Flores <antonioflores30@hotmail.com>
2023-12-05 11:06:55 -0600

    Add files via upload

---
+++ pruebaModelo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Trip = require('./models/trip.model');
+
+(async () =>{
+    await mongoose.connect("mongodb://mongo:EE1ececB4aa15-4HDAFGd3FFcg1B3ECD@roundhouse.proxy.rlwy.net:36489");
+    //await mongoose.connect("mongodb://localhost:27017/familyTrips");
+    const newTrip = await Trip.create({
+        name: 'prueba de viaje',
+        description: 'prueba de desc',
+        destination: 'Berlin',
+        category: 'amigos',
+        start_date: '2022-05-02'
+    });
+
+    console.log(newTrip);
+})();
