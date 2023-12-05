9237d985008c2e943fee601f7f9c022800c8890f
J. A Flores <antonioflores30@hotmail.com>
2023-12-05 11:18:39 -0600

    Update pruebaModelo.js
    update credencials

--- pruebaModelo.js
+++ pruebaModelo.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Trip = require('./models/trip.model');
 
 (async () =>{
-    await mongoose.connect("mongodb://mongo:EE1ececB4aa15-4HDAFGd3FFcg1B3ECD@roundhouse.proxy.rlwy.net:36489");
+    //await mongoose.connect("mongodb://mongo:EE1ececB4aa15-4HDAFGd3FFcg1B3ECD@roundhouse.proxy.rlwy.net:36489");
+    await mongoose.connect("mongodb://mongo:HE2A1hCEGgbAfbBcG6c4gCfFA4hAG6-3@viaduct.proxy.rlwy.net:28070");
+
     //await mongoose.connect("mongodb://localhost:27017/familyTrips");
     const newTrip = await Trip.create({
         name: 'prueba de viaje',
